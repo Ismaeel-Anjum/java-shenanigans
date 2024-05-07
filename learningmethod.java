490f4fb08a807fd4923ed5d58c9536e7f5f7d4c2
Ismaeel Anjum <156631563+Ismaeel-Anjum@users.noreply.github.com>
2024-05-07 19:10:23 -0400

    Add files via upload

---
+++ learningmethod.java
@@ -0,0 +1,12 @@
+public class learningmethod {
+    public static void ismaeelsmethod() {
+        int e = 13;
+        int p = 27;
+        int i = e*p;
+        System.out.println(e + " multiplied by " + p + " equals " + i);
+    }
+
+    public static void main(String[] args) {
+        ismaeelsmethod();
+    }
+}
